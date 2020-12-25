125bba9f0bb35a04194723c349fb17dba5c7ff4a
Marwan Galal <marawangalal84@gmail.com>
2020-12-25 23:19:31 +0200

    ..

--- webApi/src/main/java/com/NotificationSystem/webApi/TemplateController.java
+++ webApi/src/main/java/com/NotificationSystem/webApi/TemplateController.java
@@ -1,8 +1,33 @@
 package com.NotificationSystem.webApi;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
-public class TemplateController {
+public class TemplateController implements BController {
+    private TemplateRepository templateRepository;
+
+    @Override
+    @GetMapping("/Template/{id}")
+    public Template get(@PathVariable int id) {
+        return templateRepository.findById(id).orElse(null);
+    }
+
+
+    @PostMapping("/Template")
+    public int create(Template obj) {
+        return 0;
+    }
+
+    @Override
+    public boolean update(int id, Template newObj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
 
 }
