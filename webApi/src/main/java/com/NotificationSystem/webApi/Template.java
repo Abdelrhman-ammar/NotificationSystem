13c7715a30ed70e4d619e566f3869c55ce127a40
Marwan Galal <marawangalal84@gmail.com>
2020-12-25 22:37:53 +0200

    Template class implementation

--- webApi/src/main/java/com/NotificationSystem/webApi/Template.java
+++ webApi/src/main/java/com/NotificationSystem/webApi/Template.java
@@ -1,4 +1,55 @@
 package com.NotificationSystem.webApi;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Template {
+    private String header;
+    private String conent;
+    private Language lang;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    public Template(String header, String conent, Language lang, int id) {
+        this.header = header;
+        this.conent = conent;
+        this.lang = lang;
+        this.id = id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getConent() {
+        return conent;
+    }
+
+    public Language getLang() {
+        return lang;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setConent(String conent) {
+        this.conent = conent;
+    }
+
+    public void setLang(Language lang) {
+        this.lang = lang;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
