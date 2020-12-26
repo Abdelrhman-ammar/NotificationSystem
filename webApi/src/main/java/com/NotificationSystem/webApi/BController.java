f770905286e4cb795b5df707cc0a8bd8377bb4c1
Eyadzz <eyadyoussef1336@gmail.com>
2020-12-26 22:47:13 +0200

    updated return type

--- webApi/src/main/java/com/NotificationSystem/webApi/BController.java
+++ webApi/src/main/java/com/NotificationSystem/webApi/BController.java
@@ -1,7 +1,7 @@
 package com.NotificationSystem.webApi;
 
 public interface BController {
-    Template get(int id);
-    boolean update(Template newObj);
-    boolean delete(int id);
+    String get(int id);
+    String update(Template newObj);
+    String delete(int id);
 }
