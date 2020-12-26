cff489adb43a4d2d842d0861f6b4f4f6c7e2d5b9
Marwan Galal <marawangalal84@gmail.com>
2020-12-26 13:24:15 +0200

    Implementation of function get(int) in TemplateController class

--- webApi/src/main/java/com/NotificationSystem/webApi/TemplateController.java
+++ webApi/src/main/java/com/NotificationSystem/webApi/TemplateController.java
@@ -1,28 +1,22 @@
 package com.NotificationSystem.webApi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TemplateController implements BController {
+    @Autowired
     private TemplateRepository templateRepository;
 
     @Override
     @GetMapping("/Template")
     public Template get(@RequestParam int id) {
 
-        return (Template) templateRepository.findAll();
+        return templateRepository.findById(id).orElse(null);
     }
 
-    @GetMapping("/")
-    public String getAll(){return "done";}
-
-
-    @GetMapping("/t")
-    public int create() {
-        Template obj = new Template("hello" , "email" , Language.En);
-        templateRepository.save(obj);
-        return 0;
-    }
 
     @Override
     public boolean update(int id, Template newObj) {
