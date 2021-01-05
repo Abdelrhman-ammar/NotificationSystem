71d5cff0287913dab88d1a8a7e53f45da9f4c33a
Eyadzz <eyadyoussef1336@gmail.com>
2021-01-06 00:33:57 +0200

    added email sender

--- webApi/src/main/java/com/NotificationSystem/webApi/EmailController.java
+++ webApi/src/main/java/com/NotificationSystem/webApi/EmailController.java
@@ -2,6 +2,7 @@ package com.NotificationSystem.webApi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,10 @@ public class EmailController implements Controller{
     @Autowired
     private EmailRepository repositoryObj;
 
-
+@Autowired
+    JavaMailSender sender;
+    @Autowired
+    private NotificationRepository n1;
 
     @Override
     @GetMapping("/Email")
@@ -45,9 +49,33 @@ public class EmailController implements Controller{
     @GetMapping("/Email/send")
     public String send(@RequestParam int id) {
         Email obj = repositoryObj.getOne(id);
+        Notification n = n1.findById(obj.getNotificationId());
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("notificationsysapi@gmail.com");
+        message.setTo(obj.getEmail());
+        message.setSubject(n.getHeader());
+        message.setText(n.getContent());
+        sender.send(message);
         obj.setSendSuccessfully(true);
         update(obj);
         return "Email With id = " + id + " send Successfully";
+    }
+
+    @GetMapping("/Email/sendall")
+    public String sendAll() {
+        for(int i=1; i<repositoryObj.count(); i++) {
+            Email obj = repositoryObj.getOne(i);
+            Notification n = n1.findById(obj.getNotificationId());
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("notificationsysapi@gmail.com");
+            message.setTo(obj.getEmail());
+            message.setSubject(n.getHeader());
+            message.setText(n.getContent());
+            sender.send(message);
+            obj.setSendSuccessfully(true);
+            update(obj);
+        }
+        return "Emails were send successfully";
     }
 
     @GetMapping("/Email/all")
